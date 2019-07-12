c396b8412bd370a910b422673728cdf3221df0b4
goodlife <christopherndugo@gmail.com>
2019-07-12 20:41:26 +0300

    Add Konfetti library on login

--- app/src/main/java/ke/co/testpinterestlogin/MainActivity.java
+++ app/src/main/java/ke/co/testpinterestlogin/MainActivity.java
@@ -1,14 +1,46 @@
 package ke.co.testpinterestlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton appCompatButtonSignInOrKonfetti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final KonfettiView konfettiView = findViewById(R.id.konfettiView);
+        appCompatButtonSignInOrKonfetti= findViewById(R.id.buttonSignOrKonfetti);
+
+        appCompatButtonSignInOrKonfetti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                konfettiView.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.RECT, Shape.CIRCLE)
+                        .addSizes(new Size(12, 5f))
+                        .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                        .streamFor(300, 5000L);
+            }
+        });
     }
+
+
+
+
 }
